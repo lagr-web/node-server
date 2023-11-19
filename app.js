bd5c79e7e6e1bf8c989b7d23ad27b5d08507f9ae
Lars Gregersen <lagr@videndjurs.dk>
2023-11-19 19:41:17 +0100

    node setup express

---
+++ app.js
@@ -0,0 +1,16 @@
+// app.js
+
+import express from "express";
+import { PORT } from "./config.js";
+import routes from './routes.js'
+
+//const port = process.env.PORT || 3001;
+
+const app = express();
+
+app.use('/', routes);
+
+
+app.listen(PORT, () => {
+console.log("Server Listening and is ready on PORT:", PORT);
+});
---
+++ routes.js
@@ -0,0 +1,15 @@
+
+import express from "express";
+import morgan from 'morgan';
+import bodyParser from "body-parser";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const router = express.Router();
+
+router.get("/test", (request, response) => {
+  response.send("Hello, World root!");
+});
+
+export default router;
