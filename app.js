54aeb9a194cf5e70a7c2a04e27446d1f5d72109e
Lars Gregersen <lagr@videndjurs.dk>
2024-02-09 15:46:57 +0100

    updates router

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ app.use('/', routes);
 
 const port = process.env.PORT || 3000;
 
-app.listen(ChoosePort, () => {
+app.listen(port, () => {
 console.log("Server Listening and is ready on PORT:", port);
 });
